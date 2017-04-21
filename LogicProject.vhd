335304f4ccfeb126e291d47179f21c92f1d7dcef
oaktrunks <TomeiDLT@gmail.com>
2017-04-20 22:33:47 -0500

    Update LogicProject.vhd

--- LogicProject.vhd
+++ LogicProject.vhd
@@ -6,14 +6,14 @@ enADD, enXOR, enMOVREGTOREG, enMOVDATA, enINC, enDEC, enROL, enROR, enNEG, enOUT
 end;
 architecture decoder of LogicProject is
 begin
-enADD <=
-enXOR <=
-enMOVREGTOREG <=
-enMOVDATA <=
-enINC <=
-enDEC <=
-enROL <=
-enROR <=
-enNEG <=
-enOUT <=
+enADD <= instruction(15) and instruction(14) and instruction(13) and instruction(12) and instruction(11) and instruction(10) and instruction(9) and instruction(8) and instruction(7) and instruction(6) and instruction(5) and instruction(4) and instruction(3) and instruction(2) and instruction(1) and instruction(0)
+enXOR <= instruction(15) and instruction(14) and instruction(13) and instruction(12) and instruction(11) and instruction(10) and instruction(9) and instruction(8) and instruction(7) and instruction(6) and instruction(5) and instruction(4) and instruction(3) and instruction(2) and instruction(1) and instruction(0)
+enMOVREGTOREG <= instruction(15) and instruction(14) and instruction(13) and instruction(12) and instruction(11) and instruction(10) and instruction(9) and instruction(8) and instruction(7) and instruction(6) and instruction(5) and instruction(4) and instruction(3) and instruction(2) and instruction(1) and instruction(0)
+enMOVDATA <= instruction(15) and instruction(14) and instruction(13) and instruction(12) and instruction(11) and instruction(10) and instruction(9) and instruction(8) and instruction(7) and instruction(6) and instruction(5) and instruction(4) and instruction(3) and instruction(2) and instruction(1) and instruction(0)
+enINC <= instruction(15) and instruction(14) and instruction(13) and instruction(12) and instruction(11) and instruction(10) and instruction(9) and instruction(8) and instruction(7) and instruction(6) and instruction(5) and instruction(4) and instruction(3) and instruction(2) and instruction(1) and instruction(0)
+enDEC <= instruction(15) and instruction(14) and instruction(13) and instruction(12) and instruction(11) and instruction(10) and instruction(9) and instruction(8) and instruction(7) and instruction(6) and instruction(5) and instruction(4) and instruction(3) and instruction(2) and instruction(1) and instruction(0)
+enROL <= instruction(15) and instruction(14) and instruction(13) and instruction(12) and instruction(11) and instruction(10) and instruction(9) and instruction(8) and instruction(7) and instruction(6) and instruction(5) and instruction(4) and instruction(3) and instruction(2) and instruction(1) and instruction(0)
+enROR <= instruction(15) and instruction(14) and instruction(13) and instruction(12) and instruction(11) and instruction(10) and instruction(9) and instruction(8) and instruction(7) and instruction(6) and instruction(5) and instruction(4) and instruction(3) and instruction(2) and instruction(1) and instruction(0)
+enNEG <= instruction(15) and instruction(14) and instruction(13) and instruction(12) and instruction(11) and instruction(10) and instruction(9) and instruction(8) and instruction(7) and instruction(6) and instruction(5) and instruction(4) and instruction(3) and instruction(2) and instruction(1) and instruction(0)
+enOUT <= instruction(15) and instruction(14) and instruction(13) and instruction(12) and instruction(11) and instruction(10) and instruction(9) and instruction(8) and instruction(7) and instruction(6) and instruction(5) and instruction(4) and instruction(3) and instruction(2) and instruction(1) and instruction(0)
 end;
