d10a1b00912db615c3072d1354937f2adfdd65c7
oaktrunks <TomeiDLT@gmail.com>
2017-04-20 22:25:56 -0500

    Update LogicProject.vhd

--- LogicProject.vhd
+++ LogicProject.vhd
@@ -1,1 +1,20 @@
-braden is lame in STD_LOGIC
+library IEEE; use IEEE.STD_Logic_1164.all;
+entity LogicProject is
+port(instruction: in STD_LOGIC_VECTOR (15 downto 0);
+EXE, UPD: in STD_LOGIC;
+UPD, enADD, enXOR, enMOVREGTOREG, enMOVDATA, enINC, enDEC, enROL, enROR, enNEG, enOUT: out STD_LOGIC;
+end;
+architecture decoder of LogicProject is
+begin
+UPD <=
+enADD <=
+enXOR <=
+enMOVREGTOREG <=
+enMOVDATA <=
+enINC <=
+enDEC <=
+enROL <=
+enROR <=
+enNEG <=
+enOUT <=
+end;
