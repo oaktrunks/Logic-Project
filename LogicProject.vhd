c6101837b19be6dadfabcba4b73252819ef00bee
Braden Harrelson <bradentharrelson@gmail.com>
2017-04-25 03:28:42 -0500

    Some housekeeping

--- LogicProject.vhd
+++ LogicProject.vhd
@@ -22,34 +22,24 @@ enNEG <= instruction(15) and instruction(14) and instruction(13) and instruction
 enOUT <= instruction(15) and instruction(14) and instruction(13) and not instruction(12) and not instruction(11) and instruction(10) and instruction(9) and not instruction(8) and instruction(7) and instruction(6) and not instruction(5) and not instruction(4) and not instruction(3);
 end;
 --beginning of register 1
-architecture rtl of basic_shift_register is
-
+architecture reg1 of basic_shift_register is
 	-- Build an array type for the shift register
 	type sr_length is array ((NUM_STAGES-1) downto 0) of std_logic;
-
 	-- Declare the shift register signal
 	signal sr: sr_length;
-
 begin
-
 	process (clk)
 	begin
 		if (rising_edge(clk)) then
-
 			if (enable = '1') then
-
 				-- Shift data by one stage; data from last stage is lost
 				sr((NUM_STAGES-1) downto 1) <= sr((NUM_STAGES-2) downto 0);
-
 				-- Load new data into the first stage
 				sr(0) <= sr_in;
-
 			end if;
 		end if;
 	end process;
-
 	-- Capture the data from the last stage, before it is lost
 	sr_out <= sr(NUM_STAGES-1);
-
-end rtl;
+end reg1;
 
