18598a1f4a02e967949d48444156c097977bdf3d
Tyler Coleman <tylerblake.tc@gmail.com>
2017-04-29 17:00:20 -0500

    Message

--- LogicProject.vhd
+++ LogicProject.vhd
@@ -21,25 +21,83 @@ enROR <= instruction(15) and instruction(14) and not instruction(13) and instruc
 enNEG <= instruction(15) and instruction(14) and instruction(13) and instruction(12) and not instruction(11) and instruction(10) and instruction(9) and not instruction(8) and instruction(7) and instruction(6) and not instruction(5) and instruction(4) and instruction(3);
 enOUT <= instruction(15) and instruction(14) and instruction(13) and not instruction(12) and not instruction(11) and instruction(10) and instruction(9) and not instruction(8) and instruction(7) and instruction(6) and not instruction(5) and not instruction(4) and not instruction(3);
 end;
---beginning of register 1
-architecture reg1 of basic_shift_register is
-	-- Build an array type for the shift register
-	type sr_length is array ((NUM_STAGES-1) downto 0) of std_logic;
-	-- Declare the shift register signal
-	signal sr: sr_length;
+
+--Adder
+library IEEE; use IEEE.STD_Logic_1164.all;
+entity adder is
+
+	port 
+	(
+		a	: in std_logic_vector (7 downto 0);
+		b	: in std_logic_vector (7 downto 0);
+		cin	: in std_logic;
+		sum	: out std_logic_vector (7 downto 0);
+		cout	: out std_logic
+		
+	);
+
+end entity;
+
+architecture sum of adder is
+signal c1,c2,c3,c4,c5,c6,c7: std_logic;
 begin
-	process (clk)
+
+	sum(0) <= a(0) xor b(0) xor cin;
+	c1 <= (a(0) and b(0)) or (a(0) and cin) or (b(0) and cin);
+	
+	sum(1) <= a(1) xor b(1) xor c1;
+	c2 <= (a(1) and b(1)) or (a(1) and c1) or (b(1) and c1);
+	
+	sum(2) <= a(2) xor b(2) xor c2;
+	c3 <= (a(2) and b(2)) or (a(2) and c2) or (b(2) and c2);
+	
+	sum(3) <= a(3) xor b(3) xor c3;
+	c4 <= (a(3) and b(3)) or (a(3) and c3) or (b(3) and c3);
+	
+	sum(4) <= a(4) xor b(4) xor c4;
+	c5 <= (a(4) and b(4)) or (a(4) and c4) or (b(4) and c4);
+	
+	sum(5) <= a(5) xor b(5) xor c5;
+	c6 <= (a(5) and b(5)) or (a(5) and c5) or (b(5) and c5);
+	
+	sum(6) <= a(6) xor b(6) xor c6;
+	c7 <= (a(6) and b(6)) or (a(6) and c6) or (b(6) and c6);
+	
+	sum(7) <= a(7) xor b(7) xor c7;
+	cout <= (a(7) and b(7)) or (a(7) and c7) or (b(7) and c7);
+	
+end sum;
+
+--8 Bit register
+library IEEE; use IEEE.STD_Logic_1164.all;
+entity reg8bit is
+
+	port 
+	(
+		enADD		: in std_logic;
+		rin      : in std_logic_vector (7 downto 0);
+		rout		: out std_logic_vector (7 downto 0);
+		routnot	: out std_logic_vector (7 downto 0)
+	);
+
+end entity;
+architecture r8 of reg8bit is
+	
+	signal rt: std_logic_vector (7 downto 0);
+
+begin
+
+	process (enADD)
 	begin
-		if (rising_edge(clk)) then
-			if (enable = '1') then
-				-- Shift data by one stage; data from last stage is lost
-				sr((NUM_STAGES-1) downto 1) <= sr((NUM_STAGES-2) downto 0);
-				-- Load new data into the first stage
-				sr(0) <= sr_in;
-			end if;
+		
+		if (enADD = '1') then
+
+				rout <= rin;
+				routnot <= not rin;
+
 		end if;
 	end process;
-	-- Capture the data from the last stage, before it is lost
-	sr_out <= sr(NUM_STAGES-1);
-end reg1;
+
+	
+end r8;
 
