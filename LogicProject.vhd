fe53df0032db356138b19664e4027dc4718249ed
oaktrunks <TomeiDLT@gmail.com>
2017-04-29 20:51:06 -0500

    fixed rotator (?)

--- LogicProject.vhd
+++ LogicProject.vhd
@@ -105,24 +105,23 @@ entity rotator is
 
 	port 
 	(
-		a	: in std_logic_vector (7 downto 0)
+		a	: in std_logic_vector (7 downto 0);
+		b	: out std_logic_vector (7 downto 0)
 	);
 
 end entity;
 
 architecture leftRotator of rotator is
-signal temp : in std_logic;
 begin
-
-	temp <= a(7);
-	a(7) <= a(6);
-	a(6) <= a(5);
-	a(5) <= a(4);
-	a(4) <= a(3);
-	a(3) <= a(2);
-	a(2) <= a(1);
-	a(1) <= a(0);
-	a(0) <= temp;
+	
+	b(7) <= a(6);
+	b(6) <= a(5);
+	b(5) <= a(4);
+	b(4) <= a(3);
+	b(3) <= a(2);
+	b(2) <= a(1);
+	b(1) <= a(0);
+	b(0) <= a(7);
 	
 end leftRotator;
 
@@ -130,15 +129,14 @@ architecture rightRotator of rotator is
 signal temp : in std_logic;
 begin
 
-	temp <= a(0);
-	a(0) <= a(1);
-	a(1) <= a(2);
-	a(2) <= a(3);
-	a(3) <= a(4);
-	a(4) <= a(5);
-	a(5) <= a(6);
-	a(6) <= a(7);
-	a(7) <= temp;
+	b(0) <= a(1);
+	b(1) <= a(2);
+	b(2) <= a(3);
+	b(3) <= a(4);
+	b(4) <= a(5);
+	b(5) <= a(6);
+	b(6) <= a(7);
+	b(7) <= a(0);
 	
 end rightRotator;
 
